789b4bd220d67845eecedf19a9863f00d1cc4c02
kenei <kevinkenei9@gmail.com>
2020-05-16 23:42:05 +0300

    project setup

--- app/src/main/java/com/example/boltclone/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/example/boltclone/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.example.boltclone
+package com.example.boltclone.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.boltclone.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
